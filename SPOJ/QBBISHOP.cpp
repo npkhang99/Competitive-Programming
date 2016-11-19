5f4b7194e3e84928281901273c20e49677a6be15
01244855154 <curoa99@gmail.com>
2016-11-19 09:11:41 +0700

    Update

---
+++ SPOJ/QBBISHOP.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int,int> ii;
+
+#define x first
+#define y second
+
+const int N = 209;
+const int dx[] = {1,1,-1,-1};
+const int dy[] = {-1,1,1,-1};
+
+int n, m, p, q, s, t, a[N][N]={};
+bool b[N][N]={};
+
+int canGo(int u, int v){
+    return 1 <= u && u <= n && 1 <= v && v <= n && !b[u][v] && !a[u][v];
+}
+
+void BFS(){
+    queue<ii> qu;
+    qu.push(ii(p,q)); b[p][q] = true;
+    while(!qu.empty()){
+        ii u = qu.front(); qu.pop();
+        for(int i=0; i<4; i++){
+            for(int k=1; k<=n; k++){
+                ii v(u.x + dx[i]*k, u.y + dy[i]*k);
+                if(canGo(v.x,v.y)){
+                    a[v.x][v.y] = a[u.x][u.y] + 1;
+                    if(v == ii(s,t)) return;
+                    qu.push(v);
+                }else break;
+            }
+        }
+    }
+}
+
+int main(){
+    scanf("%d%d%d%d%d%d",&n,&m,&p,&q,&s,&t);
+    for(int i=0; i<m; i++){
+        int x, y; scanf("%d%d",&x,&y);
+        b[x][y] = true;
+    }
+
+    BFS();
+    printf("%d\n",!a[s][t]? -1:a[s][t]);
+    return 0;
+}
