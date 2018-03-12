db3f12d73459c2b08b496c4360b021ca7377abfa
Nguyen Phuc Khang <curoa99@gmail.com>
2018-03-12 19:28:22 +0700

    Reformat

--- School/BFS1.cpp
+++ School/BFS1.cpp
@@ -3,21 +3,26 @@
 #include <vector>
 using namespace std;
 
-const int N=100009;
+const int N = 100009;
 
-int n, m, d[N]={};
+int n, m, d[N] = {};
 vector<int> a[N];
 
 void BFS(){
     queue<int> q;
-    for(int i=1; i<=n; i++) d[i] = 10E8;
+    for(int i = 1; i <= n; i++){
+        d[i] = 10E8;
+    }
+
     q.push(1); d[1] = 0;
     while(!q.empty()){
         int u = q.front(); q.pop();
-        for(int i=0; i<a[u].size(); i++){
+        for(int i = 0; i < a[u].size(); i++){
             int v = a[u][i];
-            if(d[u]+1 < d[v])
-                d[v] = d[u]+1, q.push(v);
+            if(d[u] + 1 < d[v]){
+                d[v] = d[u]+1;
+                q.push(v);
+            }
         }
     }
 }
@@ -25,14 +30,16 @@ void BFS(){
 int main(){
     // freopen("BFS1.inp","r",stdin);
     // freopen("BFS1.out","w",stdout);
-    scanf("%d%d",&n,&m);
-    for(int i=0; i<m; i++){
-        int x,y; scanf("%d%d",&x,&y);
+    scanf("%d%d", &n, &m);
+    for(int i = 0; i < m; i++){
+        int x,y; scanf("%d%d", &x, &y);
         a[x].push_back(y);
         a[y].push_back(x);
     }
+    
     BFS();
-    for(int i=1; i<=n; i++)
-        printf("%d\n",d[i]==int(10E8)? -1:d[i]);
+
+    for(int i = 1; i <= n; i++)
+        printf("%d\n", d[i] == int(10E8) ? -1:d[i]);
     return 0;
 }
--- School/BFS2.cpp
+++ School/BFS2.cpp
@@ -5,27 +5,36 @@
 #include <algorithm>
 using namespace std;
 
-const int N=100009;
+const int N = 100009;
 
-int n, f, d[N]={}, tr[N]={};
+int n, f, d[N] = {}, tr[N] = {};
 vector<int> a[N], path;
 
-void BFS(int s, int last = 0){
+void BFS(int s, int last){
     queue<int> q;
-    for(int i=1; i<=n; i++) d[i] = 10E8, tr[i] = 0;
+    for(int i = 1; i <= n; i++){
+        d[i] = 10E8;
+        tr[i] = 0;
+    }
+
     d[s] = 1; q.push(s);
     while(!q.empty()){
         int u = q.front(); q.pop();
-        for(int i=0; i<a[u].size(); i++){
+        for(int i = 0; i<a[u].size(); i++){
             int v = a[u][i];
-            if(d[u]+1 < d[v]){
-                q.push(v), d[v] = d[u]+1, tr[v] = u;
+            if(d[u] + 1 < d[v]){
+                q.push(v);
+                d[v] = d[u] + 1;
+                tr[v] = u;
                 if(d[f] < d[v]) f = v;
             }
         }
     }
-    if(f!=last) BFS(f,s);
-    else printf("%d\n",d[last]), f = last;
+    
+    if(f == last){
+        printf("%d\n", d[last]);
+        f = last;
+    } else BFS(f, s);
 }
 
 void getPath(int s){
@@ -38,24 +47,34 @@ void getPath(int s){
 int main(){
     // freopen("BFS2.inp","r",stdin);
     // freopen("BFS2.out","w",stdout);
-    scanf("%d",&n);
-    for(int i=0; i<n-1; i++){
-        int x,y; scanf("%d%d",&x,&y);
+    scanf("%d", &n);
+    for(int i = 0; i < n-1; i++){
+        int x, y; scanf("%d%d", &x, &y);
         a[x].push_back(y);
         a[y].push_back(x);
     }
-    BFS(1);
+    
+    BFS(1,0);
     getPath(f);
+
     int len=10E8;
     vector<int> ans;
-    sort(path.begin(),path.end());
-    for(int i:path)
-        if(len > max(d[i]-1,d[f]-d[i]))
-            ans.clear(), ans.push_back(i), len = max(d[i]-1,d[f]-d[i]);
-        else if(len == max(d[i]-1,d[f]-d[i]))
-            ans.push_back(i);
-    printf("%d\n",(int)ans.size());
-    for(int i=0; i<ans.size(); i++) printf("%d ",ans[i]);
+
+    sort(path.begin(), path.end());
+    for(int i = 0; i < path.size(); i++){
+        int u = path[i];
+        if(len > max(d[u] - 1, d[f] - d[u])){
+            ans.clear();
+            ans.push_back(u);
+            len = max(d[u] - 1, d[f] - d[u]);
+        } else if(len == max(d[u] - 1, d[f] - d[u])){
+            ans.push_back(u);
+        }
+    }
+
+    printf("%d\n", ans.size());
+    for(int i = 0; i < ans.size(); i++)
+        printf("%d ",ans[i]);
     printf("\n");
     return 0;
 }
