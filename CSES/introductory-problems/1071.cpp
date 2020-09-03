4a726e3926d1cd8c6b7a95a74a98d45628bd1e8e
Phuc-Khang Nguyen <npkhang99@gmail.com>
2020-09-03 13:57:06 +0700

    Add CSES/1071

--- CSES/introductory-problems/1071.cpp
+++ CSES/introductory-problems/1071.cpp
@@ -7,18 +7,35 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int x, y;
+        long long x, y;
         cin >> x >> y;
-        int level = max(x, y) - 1;
-        int pos = min(x, y);
-        long long answer = 1ll * level * level + 1;
-        if (x == y) {
-            cout << answer + (level + 1) << endl;
+        long long level = max(x, y);
+
+        long long lower = (level - 1) * (level - 1) + 1;
+        long long upper = level * level;
+        long long mid = (upper + lower) / 2;
+
+        if (level % 2 == 0) {
+            if (x > y) {
+                cout << upper - y + 1 << endl;
+            }
+            else if (x < y) {
+                cout << lower + x - 1 << endl;
+            }
+            else {
+                cout << mid << endl;
+            }
         }
         else {
-            answer += (level + 1) * (1 - level % 2);
-            answer += pos;
-            cout << answer << endl;
+            if (x > y) {
+                cout << lower + y - 1 << endl;
+            }
+            else if (x < y) {
+                cout << upper - x + 1 << endl;
+            }
+            else {
+                cout << mid << endl;
+            }
         }
     }
     return 0;
